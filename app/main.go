8c093cc64212a806791b4f7982fb5e26b8da7a32
tachu <roatyungtatyu@gmail.com>
2026-03-06 23:09:55 +0900

    update server

--- app/main.go
+++ app/main.go
@@ -3,17 +3,42 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net"
 	"net/http"
+	"os"
 )
 
 type Response struct {
-	Message string `json:"message"`
+	Message   string `json:"message"`
+	Hostname  string `json:"hostname"`
+	ServerIP  string `json:"server_ip"`
+}
+
+// getServerIP はこのコンテナ（サーバー）のIPアドレスを返す
+func getServerIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hostname, _ := os.Hostname()
 		w.Header().Set("Content-Type", "application/json")
-		response := Response{Message: "Hello World"}
+		response := Response{
+			Message:  "Hello World",
+			Hostname: hostname,
+			ServerIP: getServerIP(),
+		}
 		json.NewEncoder(w).Encode(response)
 	})
 
