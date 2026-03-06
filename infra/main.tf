187639c7005a1b4408c3a1039018663cc53456d1
tachu <roatyungtatyu@gmail.com>
2026-03-06 22:19:29 +0900

    fix error

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "miniecs-terraform-state"
     key     = "terraform.tfstate"
-    region  = var.region
+    region  = "ap-northeast-1"
     encrypt = true
   }
 
